864c86f7c26ed8b5a5e21fd796b095436d83992d
shivamp20 <shivampandey.pandey8@gmail.com>
2020-06-29 20:39:28 +0530

    First commit

---
+++ src/build/precache-manifest.8d77724b1f884ff9fb6677e9fe376535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f2b1f16f65721c5b9fda0c7e96b5d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248fb035235148160931",
+    "url": "/static/css/main.7305a0d0.chunk.css"
+  },
+  {
+    "revision": "185057e8aacfe2375c8e",
+    "url": "/static/js/2.5f29287b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f29287b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248fb035235148160931",
+    "url": "/static/js/main.d403a7b0.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
